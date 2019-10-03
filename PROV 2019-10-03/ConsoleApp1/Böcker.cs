b041090151760942e5ba629105418a46e797176c
oliver176 <55237259+oliver176@users.noreply.github.com>
2019-10-03 13:40:51 +0200

    Final Commit

---
+++ PROV 2019-10-03/ConsoleApp1/Böcker.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    internal class Böcker
+    {
+        public float price;
+        private string category;
+        private int actualValue;
+        private bool cursed;
+        private int rarity;
+        private string namn;
+        private Random generator = new Random();
+
+        public Böcker(string namnInput)
+        {   
+            // Skapar string listor
+            List<string> namnLista = new List<string>(new string[] { "Flygande BOB", "Alexander Eken", "Sirdock", "Spaghettins återkomst" });
+            List<string> categoryLista = new List<string>(new string[] { "Science Fiction", "Fantasy", "Romance", "Drama" });
+
+            //Ger variablerna ett random namn/category
+            namn = namnLista[generator.Next(namnLista.Count)];
+            category = categoryLista[generator.Next(categoryLista.Count)];
+
+            //namn = namnInput;     //om du ska sätta namn genom input
+            actualValue = generator.Next(1, 100);
+            rarity = generator.Next(1, 100);
+            price = Evaluate();
+
+            if (generator.Next(100) < 50) //50% av tiden
+            {
+                cursed = false;
+            }
+            else cursed = true;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine("Book name: " + namn);
+            Console.WriteLine("Book rarity: " + rarity + "/100");
+            Console.WriteLine("Book category: " + category);
+            Console.WriteLine("Book price: " + price + "\n");
+        }
+
+        public float Evaluate()
+        {
+            float rättpris = (actualValue * rarity);
+
+            rättpris *= (generator.Next(50, 150) / 100); //blir fel pga int används, byter till float
+
+            return rättpris;
+
+            //  Sedan slumpas värdet som ska returneras mellan 50% och 150% av "rätt pris".
+        }
+
+        public string GetCategory()
+        {
+            return category;
+        }
+
+        public string GetName()
+        {
+            return namn;
+        }
+
+        public bool IsCursed()
+        {
+            if (generator.Next(100) < 80) //80% av tiden
+            {
+                return cursed;
+            }
+
+            cursed = !cursed; //flippa boolen
+            return cursed;
+        }
+    }
+}
--- PROV 2019-10-03/ConsoleApp1/Program.cs
+++ PROV 2019-10-03/ConsoleApp1/Program.cs
@@ -1,16 +1,59 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            var bookList = new List<Böcker>();
+            Console.WriteLine("Hur många böcker vill du skapa?");
+            int.TryParse(Console.ReadLine(), out int antalBöcker);
 
+            for (int i = 0; i < antalBöcker; i++)
+            {
+                Console.Clear();
+                //Console.WriteLine("Namnge bok nummer: " + (i + 1));
+                bookList.Add(new Böcker(""));
+            }
+
+            Console.WriteLine("Hur många kunder vill du skapa");
+            int.TryParse(Console.ReadLine(), out int antalKunder);  //string till int
+
+            Console.Clear();
+
+            var kundList = new List<Kunder>(); //skapar Kund listan
+
+            for (int i = 0; i < antalKunder; i++)
+            {
+                kundList.Add(new Kunder());  //Skapar "antalKunder" st nya kunder i listan
+            }
+            while (true)
+            {
+                var keyRead = Console.ReadKey(true).Key;
+                Console.WriteLine("1: Inspektera böcker");
+
+                if (keyRead == ConsoleKey.D1) //om du trycker tangenten "1"
+                {
+                    for (int i = 0; i < antalBöcker; i++)
+                    {
+                        bookList.ForEach(item => item.PrintInfo()); //printinfo för varje bok i listan
+                        Console.ReadLine();
+                    }
+                }
+                else if (keyRead == ConsoleKey.D2)
+                {
+                    
+                    
+                }
+            }
+            Console.ReadLine();
         }
+
+        /* Spelaren värderar böcker och artefakter som hen köper in till butiken genom att titta på deras skick och sällsynthet.
+         * (En korrekt värdering gör sedan att spelaren kan sätta rätt pris, och förhoppningsvis göra en vinst på senare försäljning.)
+           Spelaren kan också försöka avgöra ifall varan har någon förbannelse över sig. Det ska finnas en risk att göra en felaktig bedömning.
+           Böckerna kan tillhöra olika kategorier.*/
     }
 }
